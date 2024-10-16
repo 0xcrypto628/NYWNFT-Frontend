2ddc4abe133f46b3dd22f2e044bab56850151f6c
Blockchain Future Dreamer <162518527+0xcrypto628@users.noreply.github.com>
2024-10-16 19:19:17 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    // External dependencies fix for specific modules
     config.externals.push("pino-pretty", "lokijs", "encoding"); // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+
+    // Handling SVG files with @svgr/webpack
     config.module.rules.push({
       test: /\.svg$/i,
       use: ["@svgr/webpack"],
@@ -12,19 +15,19 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "95.216.240.153",
-        port: "5000",
-        pathname: "/**",
+        hostname: "api.minato-yellowflash.com",  // Use your secure API domain
+        port: "",                                // No need to specify port for HTTPS (443 by default)
+        pathname: "/**",                         // Match all paths for the API
       },
     ],
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,  // Temporarily ignoring ESLint issues during builds
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true,   // Temporarily ignoring TypeScript errors during builds
   },
-  reactStrictMode: false,
+  reactStrictMode: false,      // Disabling React strict mode for now
 };
 
 module.exports = nextConfig;
